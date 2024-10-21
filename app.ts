a0440db8821f05133ded62c03345462919b95e3f
Martha Watson <marthacdenzer@gmail.com>
2024-10-20 21:23:02 -0600

    Added main application logic to app.ts

---
+++ app.ts
@@ -0,0 +1,27 @@
+import { mainMenuPrompt, employeePrompt } from './utils/prompts';
+import { viewEmployees, addEmployee } from './queries/employeeQueries';
+const mainMenu = async () => {
+  const { action } = await mainMenuPrompt();
+  switch (action) {
+    case 'View Employees':
+      const employees = await viewEmployees();
+      if (employees && employees.length > 0) {
+        console.table(employees);
+      } else {
+        console.log('No employees found.');
+      }
+      break;
+    case 'Add Employee':
+      const employeeData = await employeePrompt();
+      await addEmployee(employeeData);
+      break;
+    case 'Exit':
+      process.exit();
+    default:
+      console.log('Invalid action.');
+  }
+  // Recursive call to keep the menu running after each action
+  mainMenu();
+};
+// Start the application
+mainMenu();
